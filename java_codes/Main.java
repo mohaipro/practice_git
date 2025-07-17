bed0052d44fd11ed565db911d2f60ce0ae7b34f5
mohaipro <112693316+mohaipro@users.noreply.github.com>
2025-07-17 09:56:16 +0300

    Update Main.java with a comment
    adding a single line comment

--- java_codes/Main.java
+++ java_codes/Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+      // adding a comment to sound more like professional developer
       System.out.println("Hello, World!");
   }
 }
